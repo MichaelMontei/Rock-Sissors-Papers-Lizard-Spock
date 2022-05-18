10a0c7f748a60511ceef607fb7ea6d94a37249d4
MichaelMontei <code-me@hotmail.com>
2022-05-18 15:17:43 +0200

    function made for computer to randomize number to get 1 of the matching abilitis

---
+++ index.js
@@ -0,0 +1,29 @@
+//Buttons selectors
+const rockBtn = document.querySelector('rock')
+const paperBtn = document.querySelector('paper');
+const scissorsBtn = document.querySelector('scissors');
+const lizardBtn = document.querySelector('lizard');
+const spockBtn = document.querySelector('spock');
+const winBtn = document.querySelector('computerButtonChoose');
+
+//Playerpoints en Computerpoints
+let playerPoints = document.querySelector('playerPointScore');
+let computerPoints = document.querySelector('computerPointScore');
+
+//Option Player choose
+
+
+document.getElementById("rock").addEventListener("click", function(){
+
+})
+
+function computer(){
+    let computerRandom = Math.floor(Math.random() * 5);
+    if(computerRandom === 1){
+        let computerRock = computerRandom
+    
+
+    }
+
+    }
+}

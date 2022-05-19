95cf299b38ddd661947c99fde3e159c46b5a25f3
MichaelMontei <code-me@hotmail.com>
2022-05-19 12:32:53 +0200

    created function game with a switch command to check playerinput with computerinput. Styled the page with background image

--- index.js
+++ index.js
@@ -1,39 +1,187 @@
 // make array for choices computer
 let arr = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 // get the element and make a function for getting a random word
+let computerChoice
+let playerScore = 0;
+let computerScore = 0;
+
 document.getElementById('win').addEventListener("click", random);
 
 function random(){
-    let number = arr[Math.floor(Math.random() * arr.length)];
-    console.log(number);
+  computerChoice = arr[Math.floor(Math.random() * arr.length)];
+   console.log(computerChoice);
 }
+
+//functions to get the player selection from buttons
+let playerSelection;
+const blad = document.querySelector(".result");
+const computer = document.querySelector(".computer");
+const updatePlayer = document.querySelector(".playerPointScore");
+const updatecomputer = document.querySelector(".computerPointScore");
 
 document.getElementById('rock').addEventListener('click', rock);
 
 function rock() {
-    let rock = document.getElementById("rock").value;
-    console.log(rock);
+    playerSelection = "rock";
+    console.log(playerSelection);
+    random();
+    game(playerSelection, computerChoice);
 }
 
 document.getElementById('paper').addEventListener('click', paper);
 
 function paper() {
-    let paper = document.getElementById("paper").value;
-    console.log(paper);
+    playerSelection = "paper";
+    console.log(playerSelection);
+    random();
+    game(playerSelection, computerChoice);
 }
 
-document.getElementById('paper').addEventListener('click', scissors);
+document.getElementById('scissors').addEventListener('click', scissors);
 
 function scissors() {
-    let scissors = document.getElementById("scissors").value;
-    console.log(scissors);
+    playerSelection = "scissors";
+    console.log(playerSelection);
+    random();
+    game(playerSelection, computerChoice);
 }
 
+document.getElementById('lizard').addEventListener('click', lizard);
 
+function lizard() {
+    playerSelection = "lizard";
+    console.log(playerSelection);
+    random();
+    game(playerSelection, computerChoice);
+}
 
+document.getElementById('spock').addEventListener('click', spock);
 
+function spock() {
+    playerSelection = "spock";
+    console.log(playerSelection);
+    random();
+    game(playerSelection, computerChoice);
+}
 
+//check player vs computer
 
+function game(playerSelection, computerChoice){
+    switch (playerSelection) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    blad.innerText = "It's a draw!"
+                    computer.innerText = computerChoice;
+                    playerScore++;
+                    updatePlayer.innerText = playerScore;
+                    computerScore++;
+                    updatecomputer.innerText = computerScore;
+                    break;
+                case "paper":
+                    blad.innerText = "Your opponent wins!"
+                    computer.innerText = computerChoice;
+
+                    break;
+                case "scissors":
+                    blad.innerText = "You win!"
+                    computer.innerText = computerChoice;
+                    break;
+                case "lizard":
+                    blad.innerText = "You win!"
+                    computer.innerText = computerChoice;
+                    break;
+                case "spock":
+                    blad.innerText = "Your opponent wins!"
+                    computer.innerText = computerChoice;
+                    break;
+            }
+            break;
+
+        case "scissors":
+            switch (computerChoice) {
+                case "scissors":
+                    blad.innerText = "It's a draw!"
+                    break;
+                case "rock":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+                case "lizard":
+                    blad.innerText = "You win!"
+                    break;
+                case "paper":
+                    blad.innerText = "You win!"
+                    break;
+                case "spock":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+
+            }
+            break;
+
+        case "paper":
+            switch (computerChoice) {
+                case "paper":
+                    blad.innerText = "It's a draw!"
+                    break;
+                case "scissors":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+                case "rock":
+                    blad.innerText = "You win!"
+                    break;
+                case "spock":
+                    blad.innerText = "You win!"
+                    break;
+                case "lizard":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+
+            }
+            break;
+
+        case "lizard":
+            switch (computerChoice) {
+                case "lizard":
+                    blad.innerText = "It's a draw!"
+                    break;
+                case "rock":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+                case "paper":
+                    blad.innerText = "You win!"
+                    break;
+                case "spock":
+                    blad.innerText = "You win!"
+                    break;
+                case "scissors":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+
+            }
+            break;
+        case "spock":
+            switch (computerChoice) {
+                case "spock":
+                    blad.innerText = "It's a draw!"
+                    break;
+                case "lizard":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+                case "rock":
+                    blad.innerText = "You win!"
+                    break;
+                case "scissors":
+                    blad.innerText = "You win!"
+                    break;
+                case "paper":
+                    blad.innerText = "Your opponent wins!"
+                    break;
+
+            }
+            break;
+    }
+}
 
 //Buttons selectors
 
